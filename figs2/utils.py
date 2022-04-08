e17d979d89ff6d518aabdaebb4aa05bd11f0185e
Kristen Krumhardt <kristen.krumhardt@colorado.edu>
2022-04-08 10:35:14 -0600

    added new notebooks before submission

---
+++ figs2/utils.py
@@ -0,0 +1,106 @@
+import traceback
+import warnings
+
+import numpy as np
+import xarray as xr
+
+#import variable_defs
+
+nmols_to_PgCyr = 1e-9 * 86400. * 365. * 12e-15
+
+
+def global_mean(ds, normalize=True, include_ms=False):
+    """
+    Compute the global mean on a POP dataset. 
+    Return computed quantity in conventional units.
+    """
+
+    compute_vars = [
+        v for v in ds 
+        if 'time' in ds[v].dims and ('nlat', 'nlon') == ds[v].dims[-2:]
+    ]
+    other_vars = list(set(ds.variables) - set(compute_vars))
+
+    if include_ms:
+        surface_mask = ds.TAREA.where(ds.KMT > 0).fillna(0.)
+    else:
+        surface_mask = ds.TAREA.where(ds.REGION_MASK > 0).fillna(0.)        
+    
+    masked_area = {
+        v: surface_mask.where(ds[v].notnull()).fillna(0.) 
+        for v in compute_vars
+    }
+
+    with xr.set_options(keep_attrs=True):
+        
+        dso = xr.Dataset({
+            v: (ds[v] * masked_area[v]).sum(['nlat', 'nlon'])
+            for v in compute_vars
+        })
+        if normalize:
+            dso = xr.Dataset({
+                v: dso[v] / masked_area[v].sum(['nlat', 'nlon'])
+                for v in compute_vars
+            })            
+        else:
+            for v in compute_vars:
+                if v in variable_defs.C_flux_vars:
+                    dso[v] = dso[v] * nmols_to_PgCyr
+                    dso[v].attrs['units'] = 'Pg C yr$^{-1}$'
+                
+        return xr.merge([dso, ds[other_vars]]).drop(
+            [c for c in ds.coords if ds[c].dims == ('nlat', 'nlon')]
+        )
+    
+    
+def adjust_pop_grid(tlon,tlat,field):
+    nj = tlon.shape[0]
+    ni = tlon.shape[1]
+    xL = int(ni/2 - 1)
+    xR = int(xL + ni)
+
+    tlon = np.where(np.greater_equal(tlon,min(tlon[:,0])),tlon-360.,tlon)
+    lon  = np.concatenate((tlon,tlon+360.),1)
+    lon = lon[:,xL:xR]
+
+    if ni == 320:
+        lon[367:-3,0] = lon[367:-3,0]+360.
+    lon = lon - 360.
+    lon = np.hstack((lon,lon[:,0:1]+360.))
+    if ni == 320:
+        lon[367:,-1] = lon[367:,-1] - 360.
+
+    #-- trick cartopy into doing the right thing:
+    #   it gets confused when the cyclic coords are identical
+    lon[:,0] = lon[:,0]-1e-8
+    
+    #-- periodicity
+    lat  = np.concatenate((tlat,tlat),1)
+    lat = lat[:,xL:xR]
+    lat = np.hstack((lat,lat[:,0:1]))
+
+    field = np.ma.concatenate((field,field),1)
+    field = field[:,xL:xR]
+    field = np.ma.hstack((field,field[:,0:1]))
+    return lon,lat,field
+
+
+def normal_lons(lons):
+
+    lons_norm=np.full((len(lons.nlat), len(lons.nlon)), np.nan)
+
+    lons_norm_firstpart = lons.where(lons<=180.)
+    lons_norm_secpart = lons.where(lons>180.) - 360.
+
+    lons_norm_firstpart = np.asarray(lons_norm_firstpart)
+    lons_norm_secpart = np.asarray(lons_norm_secpart)
+
+    lons_norm[~np.isnan(lons_norm_firstpart)] = lons_norm_firstpart[~np.isnan(lons_norm_firstpart)]
+    lons_norm[~np.isnan(lons_norm_secpart)] = lons_norm_secpart[~np.isnan(lons_norm_secpart)]
+
+    lons_norm=xr.DataArray(lons_norm)
+    lons_norm=lons_norm.rename({'dim_0':'nlat'})
+    lons_norm=lons_norm.rename({'dim_1':'nlon'})
+    
+    return(lons_norm)
+
